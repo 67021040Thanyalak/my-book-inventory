e9d152b71584312061b9e585e415970d984b1c6f
MiKuOz1 <146723010+MiKuOz1@users.noreply.github.com>
2026-03-04 01:18:09 +0700

    fix: add export and clean prisma lib

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true, // ตัวนี้แหละจะทำให้ผ่าน
+    ignoreBuildErrors: true, 
   },
   eslint: {
     ignoreDuringBuilds: true,
