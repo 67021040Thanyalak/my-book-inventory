40be928a7f5f8a9f44ca4ceb5b5e5deabf81f128
MiKuOz1 <146723010+MiKuOz1@users.noreply.github.com>
2026-03-04 01:36:08 +0700

    fix: final build configuration for login page

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // ข้ามการตรวจ Type เพื่อไม่ให้ Prisma บล็อกการ Build
-    ignoreBuildErrors: true,
-  }
-};
-
-export default nextConfig;
