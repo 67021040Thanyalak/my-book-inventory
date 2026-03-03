cfd606ad70ba9f6f77901b42833ba038563d232c
MiKuOz1 <146723010+MiKuOz1@users.noreply.github.com>
2026-03-04 01:10:52 +0700

    fix: relax next.config and ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   typescript: {
-    // บังคับให้ Build ผ่านแม้ TypeScript จะบ่นเรื่อง PrismaClient
+    // บังคับให้ผ่านแม้จะหา PrismaClient ไม่เจอตอนตรวจโค้ด
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ข้ามการตรวจ Lint ตอน Build
+    // ข้ามการตรวจ Lint เพื่อไม่ให้เสียเวลาและลดโอกาสเกิด Error
     ignoreDuringBuilds: true,
   },
 };
